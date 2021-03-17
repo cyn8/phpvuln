7f6179abe45e581243e58ab0680245b455b057d1
Cynate <noah@dvuln.com>
2021-03-17 13:19:26 +1100

    db rebuild

--- sql_db/db.sql
+++ sql_db/db.sql
@@ -1,80 +1,88 @@
--- phpMyAdmin SQL Dump
--- version 4.8.4
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 5.7.33, for Linux (x86_64)
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 05 fév. 2019 à 08:35
--- Version du serveur :  5.7.24
--- Version de PHP :  7.2.14
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: noob
+-- ------------------------------------------------------
+-- Server version	5.7.33-0ubuntu0.18.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données :  `noob`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `chat`
+-- Table structure for table `chat`
 --
 
 DROP TABLE IF EXISTS `chat`;
-CREATE TABLE IF NOT EXISTS `chat` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `chat` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `message` text NOT NULL,
-  `de` varchar(255) NOT NULL,
+  `message` text,
+  `de` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `chat`
+-- Dumping data for table `chat`
 --
 
-INSERT INTO `chat` (`id`, `message`, `de`) VALUES
-(1, 'Salut, on test le chat ?', 'Homer'),
-(2, 'Ca marche, allé viens manger !', 'Marge'),
-(3, 'Tu passes au bar ?', 'Moe'),
-(4, 'Je ne pense vraiment pas que ca soit une bonne idée Moe... Homer doit aussi se soucier de sa famille, il n\'a toujours pas diné avec nous alors que c\'est le premier assis a table habituellement .... Je ne le comprend plus depuis qu\'il est devenu Administrateur de ce site \"Noob\" ... il ne fait que bosser .. Je ne reconnait plus mon homer :(', 'Marge'),
-(5, 'Un brave type cet Homer ...', 'Montgomery');
-
--- --------------------------------------------------------
+LOCK TABLES `chat` WRITE;
+/*!40000 ALTER TABLE `chat` DISABLE KEYS */;
+INSERT INTO `chat` VALUES (1,'Salut, on test le chat ?','Homer'),(2,'Ca marche, allÃ© viens manger !','Marge'),(3,'Tu passes au bar ?','Moe'),(4,'Je ne pense vraiment pas que ca soit une bonne idÃ©e Moe... Homer doit aussi se soucier de sa famille, il n\'a toujours pas dinÃ© avec nous alors que c\'est le premier assis a table habituellement .... Je ne le comprend plus depuis qu\'il est devenu Administrateur de ce site \"Noob\" ... il ne fait que bosser .. Je ne reconnait plus mon homer :(','Marge'),(5,'Un brave type cet Homer ...','Montgomery');
+/*!40000 ALTER TABLE `chat` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
--- Structure de la table `users`
+-- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `id` int(7) NOT NULL AUTO_INCREMENT,
-  `prenom` varchar(255) NOT NULL,
-  `nom` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  `bio` varchar(255) NOT NULL,
+  `prenom` varchar(255) DEFAULT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `bio` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `prenom`, `nom`, `password`, `image`, `bio`) VALUES
-(1, 'Homer', 'Simpson', 'd048e0f7f525f28595f4297219de507e', 'img/pic.jpg', 'C\'est pas parce que je m\'en fous que je comprends pas.'),
-(2, 'Marge', 'Simpson', '70b959c40fe68e799d54296ba32834ef', 'img/pic2.jpg', 'Quand une femme dit que tout va bien, ça veut dire que tout va mal.'),
-(5, 'Montgomery', 'Burns', '9726255eec083aa56dc0449a21b33190', 'img/pic3.jpg', 'Famille, religion, amitié. Voici les 3 démons que vous devez abattre pour réussir dans les affaires.'),
-(6, 'Moe', 'Szyslak', '3e1867f5aee83045775fbe355e6a3ce1', 'img/pic4.jpg', 'On pourrait vendre de l\'alcool. Je fais ça très bien.');
-COMMIT;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Homer','Simpson','d048e0f7f525f28595f4297219de507e','img/pic.jpg','C\'est pas parce que je m\'en fous que je comprends pas.'),(2,'Marge','Simpson','70b959c40fe68e799d54296ba32834ef','img/pic2.jpg','Quand une femme dit que tout va bien, Ã§a veut dire que tout va mal.'),(3,'Montgomery','Burns','9726255eec083aa56dc0449a21b33190','img/pic3.jpg','Famille, religion, amitiÃ©. Voici les 3 dÃ©mons que vous devez abattre pour rÃ©ussir dans les affaires.'),(4,'Moe','Szyslak','3e1867f5aee83045775fbe355e6a3ce1','img/pic4.jpg','On pourrait vendre de l\'alcool. Je fais Ã§a trÃ¨s bien.');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Dumping events for database 'noob'
+--
+
+--
+-- Dumping routines for database 'noob'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-03-17 13:18:45
